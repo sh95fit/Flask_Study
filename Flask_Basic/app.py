219abda0ad5165acf0799d67d66d1f039eae5b45
sh95fit <h95fit@gmail.com>
2023-03-16 17:44:16 +0900

    Flask - 초기 환경설정 & flask run 테스트(디버그모드 설정 등)

---
+++ Flask_Basic/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+print('__name__', __name__)
+print('DEBUG', app.config['DEBUG'])
+
+
+@app.route('/')
+def index():
+    return 'hello world!!!'
+
+
+if __name__ == "__main__":
+    print('run')
+    app.run(debug=True)
