5acd9d8f4054c5aab1ee9d3245c31aeac2915ed5
sh95fit <h95fit@gmail.com>
2023-03-20 14:04:12 +0900

    Flask - 템플릿 렌더링 기초

--- Flask_Basic/__init__.py
+++ Flask_Basic/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
+from flask import render_template
 
 # __init__.py를 통해 디렉토리 지정
-
-# 테스트 용도
-db = 'database'
 
 
 def create_app():
@@ -12,80 +10,84 @@ def create_app():
 
     @app.route('/')
     def index():
-        app.logger.info('RUN Flask Server')
-        return 'hello world!!!'
+        # app.logger.info('RUN Flask Server')
+        return render_template('index.html')
 
-    ''' Routing Practice'''
-    from flask import jsonify, redirect, url_for
-    from markupsafe import escape
+    @app.errorhandler(404)
+    def page_404(error):
+        return render_template('/404.html')
 
-    @app.route('/test/name/<name>')
-    def name(name):
-        return f'Name is {name}, {escape(type(name))}'
+    # ''' Routing Practice'''
+    # from flask import jsonify, redirect, url_for
+    # from markupsafe import escape
 
-    @app.route('/test/id/<int:id>')
-    def id(id):
-        return "ID : %d" % id
+    # @app.route('/test/name/<name>')
+    # def name(name):
+    #     return f'Name is {name}, {escape(type(name))}'
 
-    @app.route('/test/path/<path:subpath>')
-    def path(subpath):
-        return subpath
+    # @app.route('/test/id/<int:id>')
+    # def id(id):
+    #     return "ID : %d" % id
 
-    @app.route('/test/json')
-    def json():
-        return jsonify({'hello': 'flask'})
+    # @app.route('/test/path/<path:subpath>')
+    # def path(subpath):
+    #     return subpath
 
-    @app.route('/test/redirect/<path:subpath>')
-    def redirect_url(subpath):
-        return redirect(subpath)
+    # @app.route('/test/json')
+    # def json():
+    #     return jsonify({'hello': 'flask'})
 
-    @app.route('/test/urlfor/<path:subpath>')
-    def urlfor(subpath):
-        return redirect(url_for('path', subpath=subpath))
+    # @app.route('/test/redirect/<path:subpath>')
+    # def redirect_url(subpath):
+    #     return redirect(subpath)
 
-    ''' Request Hook'''
-    from flask import g, current_app
+    # @app.route('/test/urlfor/<path:subpath>')
+    # def urlfor(subpath):
+    #     return redirect(url_for('path', subpath=subpath))
 
-    @app.before_first_request
-    def before_first_request():
-        app.logger.info('BEFORE_FIRST_REQUEST')
+    # ''' Request Hook'''
+    # from flask import g, current_app
 
-    @app.before_request
-    def before_request():
-        g.test = True
-        app.logger.info('BEFORE_REQUEST')
+    # @app.before_first_request
+    # def before_first_request():
+    #     app.logger.info('BEFORE_FIRST_REQUEST')
 
-    @app.after_request
-    def after_request(response):
-        app.logger.info(f'g.test:{g.test}')
-        app.logger.info(f'current_app.config:{current_app.config}')
-        app.logger.info('AFTER_REQUEST')
-        return response
+    # @app.before_request
+    # def before_request():
+    #     g.test = True
+    #     app.logger.info('BEFORE_REQUEST')
 
-    @app.teardown_request
-    def teardown_request(exception):
-        app.logger.info('TEARDOWN_REQUEST')
+    # @app.after_request
+    # def after_request(response):
+    #     app.logger.info(f'g.test:{g.test}')
+    #     app.logger.info(f'current_app.config:{current_app.config}')
+    #     app.logger.info('AFTER_REQUEST')
+    #     return response
 
-    @app.teardown_appcontext
-    def teardown_appcontext(exception):
-        app.logger.info('TEARDOWN_CONTEXT')
+    # @app.teardown_request
+    # def teardown_request(exception):
+    #     app.logger.info('TEARDOWN_REQUEST')
 
-    '''Method'''
-    from flask import request
+    # @app.teardown_appcontext
+    # def teardown_appcontext(exception):
+    #     app.logger.info('TEARDOWN_CONTEXT')
 
-    def on_json_loading_failed_return_dict(e):
-        return {}
+    # '''Method'''
+    # from flask import request
 
-    @app.route('/test/method/<id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-    def method_test(id):
-        request.on_json_loading_failed = on_json_loading_failed_return_dict
+    # def on_json_loading_failed_return_dict(e):
+    #     return {}
 
-        return jsonify({
-            'request.method': request.method,
-            "request.args": request.args,
-            "request.form": request.form,
-            "request.json": request.json,
-        })
+    # @app.route('/test/method/<id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+    # def method_test(id):
+    #     request.on_json_loading_failed = on_json_loading_failed_return_dict
+
+    #     return jsonify({
+    #         'request.method': request.method,
+    #         "request.args": request.args,
+    #         "request.form": request.form,
+    #         "request.json": request.json,
+    #     })
 
     # return app.run(debug=True)
     return app
