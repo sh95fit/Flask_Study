2674edfcb6f0ce8b851a4408569f5dcca4bb7062
sh95fit <h95fit@gmail.com>
2023-04-26 16:26:24 +0900

    Flask - 도커 컴포즈 nginx 설정 추가

--- etc/docker-entrypoint.sh
+++ etc/docker-entrypoint.sh
@@ -3,4 +3,5 @@
 set -e
 
 flask db upgrade
-gunicorn --bind :9000 --workers 2 --threads 8 --access-logfile - 'Flask_Basic:create_app()'
+# gunicorn --bind :9000 --workers 2 --threads 8 --access-logfile - 'Flask_Basic:create_app()'
+gunicorn --bind unix:/tmp/flask_server.sock --workers 2 --threads 8 --reload --access-logfile - 'Flask_Basic:create_app()'
